4dd66e8779e519b98a3cc161afb6ec5355f06c1c
pidokige02 <pidokige0204@gmail.com>
2023-07-07 04:40:00 +0900

    New models and API

--- chat/urls.py
+++ chat/urls.py
@@ -1,10 +1,8 @@
 # chat/urls.py
-from django.urls import path
+from django.urls import path, re_path
 
-from . import views
-
+app_name = 'chat'
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<str:room_name>/", views.room, name="room"),
+
 ]
--- chatsite/urls.py
+++ chatsite/urls.py
@@ -21,7 +21,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path("chat/", include("chat.urls")),
+    path('chat/', include('chat.api.urls', namespace='chat')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
 ]
